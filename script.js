77363911ffe5c763b140160d97ff7820ced36038
Mahfuzur Rahman <mahfuz468865@gmail.com>
2021-05-14 11:25:56 +0600

    Tests done

---
+++ script.js
@@ -0,0 +1,42 @@
+//Question No:1
+
+function add7(){
+    return parseInt(prompt("Pick a number"))+7;
+ }
+ add7();
+ 
+//Question No:2
+
+ function MULTIPLY(price, shipping) {
+    //Return the sum
+    return price*shipping;
+}
+
+//Call MULTIPLY to multiply the two numbers
+var product=MULTIPLY(5,2);
+
+alert (product);
+
+//capitalize
+function capitalize(string) {
+    return string[0].toUpperCase() + string.slice(1);}
+
+    console.log(capitalize('mahfuzur rahman'));
+
+    
+//lastLetter
+    var hello = "Hello World";
+var lastLetter=hello.slice(-1);
+
+console.log(lastLetter);
+
+
+
+
+
+
+
+
+
+
+
